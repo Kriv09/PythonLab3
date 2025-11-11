f8cde48fd8f2fa4417b36a31db27155afe661461
nazar.kr <nazar.kr@nextcode.tech>
2025-11-11 23:36:09 +0200

    feat(0000): created views for LAB 5

--- bank_project/urls.py
+++ bank_project/urls.py
@@ -1,27 +1,11 @@
-"""
-URL configuration for bank_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-
-from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-
-from core.api.views import ClientViewSet, AccountTypeViewSet, BranchViewSet, AccountViewSet, TransactionTypeViewSet, \
-    TransactionViewSet, ReportView
+from rest_framework.routers import DefaultRouter
+from core.api.views import (
+    ClientViewSet, AccountTypeViewSet, BranchViewSet, AccountViewSet,
+    TransactionTypeViewSet, TransactionViewSet, ReportView
+)
+from core import views
 
 router = DefaultRouter()
 router.register(r'clients', ClientViewSet)
@@ -32,6 +16,57 @@ router.register(r'transaction-types', TransactionTypeViewSet)
 router.register(r'transactions', TransactionViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # REST API
     path('api/', include(router.urls)),
     path('api/report/', ReportView.as_view(), name='report'),
+
+    # Clients
+    path('clients/', views.ClientListView.as_view(), name='client_list'),
+    path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
+    path('clients/add/', views.ClientCreateView.as_view(), name='client_add'),
+    path('clients/<int:pk>/edit/', views.ClientUpdateView.as_view(), name='client_edit'),
+    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+
+    # Account Types
+    path('account-types/', views.AccountTypeListView.as_view(), name='accounttype_list'),
+    path('account-types/add/', views.AccountTypeCreateView.as_view(), name='accounttype_add'),
+    path('account-types/<int:pk>/', views.AccountTypeDetailView.as_view(), name='accounttype_detail'),
+    path('account-types/<int:pk>/edit/', views.AccountTypeUpdateView.as_view(), name='accounttype_edit'),
+    path('account-types/<int:pk>/delete/', views.AccountTypeDeleteView.as_view(), name='accounttype_delete'),
+
+    # Branches
+    path('branches/', views.BranchListView.as_view(), name='branch_list'),
+    path('branches/add/', views.BranchCreateView.as_view(), name='branch_add'),
+    path('branches/<int:pk>/', views.BranchDetailView.as_view(), name='branch_detail'),
+    path('branches/<int:pk>/edit/', views.BranchUpdateView.as_view(), name='branch_edit'),
+    path('branches/<int:pk>/delete/', views.BranchDeleteView.as_view(), name='branch_delete'),
+
+    # Accounts
+    path('accounts/', views.AccountListView.as_view(), name='account_list'),
+    path('accounts/add/', views.AccountCreateView.as_view(), name='account_add'),
+    path('accounts/<int:pk>/', views.AccountDetailView.as_view(), name='account_detail'),
+    path('accounts/<int:pk>/edit/', views.AccountUpdateView.as_view(), name='account_edit'),
+    path('accounts/<int:pk>/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
+
+    # Transaction Types
+    path('transaction-types/', views.TransactionTypeListView.as_view(), name='transactiontype_list'),
+    path('transaction-types/add/', views.TransactionTypeCreateView.as_view(), name='transactiontype_add'),
+    path('transaction-types/<int:pk>/', views.TransactionTypeDetailView.as_view(), name='transactiontype_detail'),
+    path('transaction-types/<int:pk>/edit/', views.TransactionTypeUpdateView.as_view(), name='transactiontype_edit'),
+    path('transaction-types/<int:pk>/delete/', views.TransactionTypeDeleteView.as_view(), name='transactiontype_delete'),
+
+    # Transactions
+    path('transactions/', views.TransactionListView.as_view(), name='transaction_list'),
+    path('transactions/add/', views.TransactionCreateView.as_view(), name='transaction_add'),
+    path('transactions/<int:pk>/', views.TransactionDetailView.as_view(), name='transaction_detail'),
+    path('transactions/<int:pk>/edit/', views.TransactionUpdateView.as_view(), name='transaction_edit'),
+    path('transactions/<int:pk>/delete/', views.TransactionDeleteView.as_view(), name='transaction_delete'),
+
+
+    # Dashboard
+    path('', views.DashboardView.as_view(), name='dashboard'),
+
 ]
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,173 @@
+from django.views import generic
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.db.models import ProtectedError
+from .models import Client, AccountType, Branch, Account, TransactionType, Transaction
 
-# Create your views here.
+class SafeDeleteView(generic.DeleteView):
+    protected_related_name = None  # Optional, for error message
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+            return super().delete(request, *args, **kwargs)
+        except ProtectedError as e:
+            return render(request, "core/pages/error.html", {
+                "message": f"Cannot delete '{self.object}' because it is used by other objects.",
+                "back_url": self.get_success_url()
+            })
+
+# --- CLIENTS ---
+class ClientListView(generic.ListView):
+    model = Client
+    template_name = "core/pages/client_list.html"
+
+class ClientDetailView(generic.DetailView):
+    model = Client
+    template_name = "core/pages/client_detail.html"
+
+class ClientCreateView(generic.CreateView):
+    model = Client
+    fields = ["full_name", "email", "phone"]
+    template_name = "core/pages/client_form.html"
+    success_url = reverse_lazy("client_list")
+
+class ClientUpdateView(ClientCreateView, generic.UpdateView):
+    pass
+
+class ClientDeleteView(SafeDeleteView):
+    model = Client
+    template_name = "core/pages/client_confirm_delete.html"
+    success_url = reverse_lazy("client_list")
+
+
+# --- ACCOUNT TYPES ---
+class AccountTypeListView(generic.ListView):
+    model = AccountType
+    template_name = "core/pages/accounttype_list.html"
+
+class AccountTypeDetailView(generic.DetailView):
+    model = AccountType
+    template_name = "core/pages/accounttype_detail.html"
+
+class AccountTypeCreateView(generic.CreateView):
+    model = AccountType
+    fields = ["type_name", "description"]
+    template_name = "core/pages/accounttype_form.html"
+    success_url = reverse_lazy("accounttype_list")
+
+class AccountTypeUpdateView(AccountTypeCreateView, generic.UpdateView):
+    pass
+
+class AccountTypeDeleteView(SafeDeleteView):
+    model = AccountType
+    template_name = "core/pages/accounttype_confirm_delete.html"
+    success_url = reverse_lazy("accounttype_list")
+
+
+# --- BRANCHES ---
+class BranchListView(generic.ListView):
+    model = Branch
+    template_name = "core/pages/branch_list.html"
+
+class BranchDetailView(generic.DetailView):
+    model = Branch
+    template_name = "core/pages/branch_detail.html"
+
+class BranchCreateView(generic.CreateView):
+    model = Branch
+    fields = ["branch_name", "city", "country"]
+    template_name = "core/pages/branch_form.html"
+    success_url = reverse_lazy("branch_list")
+
+class BranchUpdateView(BranchCreateView, generic.UpdateView):
+    pass
+
+class BranchDeleteView(SafeDeleteView):
+    model = Branch
+    template_name = "core/pages/branch_confirm_delete.html"
+    success_url = reverse_lazy("branch_list")
+
+
+# --- ACCOUNTS ---
+class AccountListView(generic.ListView):
+    model = Account
+    template_name = "core/pages/account_list.html"
+
+class AccountDetailView(generic.DetailView):
+    model = Account
+    template_name = "core/pages/account_detail.html"
+
+class AccountCreateView(generic.CreateView):
+    model = Account
+    fields = ["client", "account_type", "branch", "balance"]
+    template_name = "core/pages/account_form.html"
+    success_url = reverse_lazy("account_list")
+
+class AccountUpdateView(AccountCreateView, generic.UpdateView):
+    pass
+
+class AccountDeleteView(SafeDeleteView):
+    model = Account
+    template_name = "core/pages/account_confirm_delete.html"
+    success_url = reverse_lazy("account_list")
+
+
+# --- TRANSACTION TYPES ---
+class TransactionTypeListView(generic.ListView):
+    model = TransactionType
+    template_name = "core/pages/transactiontype_list.html"
+
+class TransactionTypeDetailView(generic.DetailView):
+    model = TransactionType
+    template_name = "core/pages/transactiontype_detail.html"
+
+class TransactionTypeCreateView(generic.CreateView):
+    model = TransactionType
+    fields = ["type_name"]
+    template_name = "core/pages/transactiontype_form.html"
+    success_url = reverse_lazy("transactiontype_list")
+
+class TransactionTypeUpdateView(TransactionTypeCreateView, generic.UpdateView):
+    pass
+
+class TransactionTypeDeleteView(SafeDeleteView):
+    model = TransactionType
+    template_name = "core/pages/transactiontype_confirm_delete.html"
+    success_url = reverse_lazy("transactiontype_list")
+
+
+# --- TRANSACTIONS ---
+class TransactionListView(generic.ListView):
+    model = Transaction
+    template_name = "core/pages/transaction_list.html"
+
+class TransactionDetailView(generic.DetailView):
+    model = Transaction
+    template_name = "core/pages/transaction_detail.html"
+
+class TransactionCreateView(generic.CreateView):
+    model = Transaction
+    fields = ["sender_account", "receiver_account", "transaction_type", "amount", "description"]
+    template_name = "core/pages/transaction_form.html"
+    success_url = reverse_lazy("transaction_list")
+
+class TransactionUpdateView(TransactionCreateView, generic.UpdateView):
+    pass
+
+class TransactionDeleteView(SafeDeleteView):
+    model = Transaction
+    template_name = "core/pages/transaction_confirm_delete.html"
+    success_url = reverse_lazy("transaction_list")
+
+
+# --- DASHBOARD ---
+class DashboardView(generic.TemplateView):
+    template_name = "core/pages/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["clients"] = Client.objects.all()
+        context["accounts"] = Account.objects.all()
+        context["transactions"] = Transaction.objects.all()
+        return context
